a40b8073c39558b662959648f1b182054e210b3b
Luiz Henrique <luizhenriquerezio@gmail.com>
2024-12-12 09:34:00 -0300

    feat: add exemplo

--- obj_function.py
+++ obj_function.py
@@ -16,6 +16,22 @@ def nowak_collins_example(x, none_variable):
 
     return [capacity], [demand], [constraint]
 
+def nowak_collins_example1(x, none_variable):
+    """Objective function for the Nowak example (tutorial).
+    """
+
+    # Random variables
+    f_y = x[0]
+    p_load = x[1]
+    w_load = x[2]
+    capacity = 80 * f_y
+    demand = 54 * p_load + 5832 * w_load
+
+    # State limit function
+    constraint = capacity - demand
+
+    return [capacity, capacity, capacity], [demand, demand, demand], [constraint, constraint, constraint]
+
 
 def nowak_collins_time_example(x, none_variable):
     """Objective function for the Nowak example (tutorial).
