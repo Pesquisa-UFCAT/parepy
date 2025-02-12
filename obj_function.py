5928e271b2506ec802d02a39c91062456247b321
wmpjrufg <wanderlei_junior@ufcat.edu.br>
2025-02-12 18:46:14 -0300

    feat: refactor and simplify objective and gradient functions in obj_function.py; remove outdated deterministic_algorithm_structural_analysis function

--- obj_function.py
+++ obj_function.py
@@ -80,24 +80,19 @@ def nowak_collins_time_example(x, none_variable):
     return [capacity], [demand], [constraint]
 
 
-def beck_example(x, none_variable):
-    y_1 = x[0]
-    y_2 = x[1]
-    y_3 = x[2]
-    # g = 12.50 * y_1 * y_2 + 100 * y_2 + 250 * y_1 - 200 * y_3 + 1000
-    g = y_1 * y_2 - y_3
-    
-    return g
+def obj_form(x, none_variable=None):
+    simga_y = x[0]
+    w = x[1]
+    m = x[2]
+    return simga_y * w - m
 
 
-def grad_beck_example(x, none_variable):
-    y_1 = x[0]
-    y_2 = x[1]
-    y_3 = x[2]
-    # g_grad = [12.50 * y_2 + 250, 12.50 * y_1 + 100, -200]
-    g_grad = [y_2, y_1, -1]
-    
-    return g_grad
+def grad_obj_form(x, none_variable=None):
+    sigma_y = x[0]
+    w = x[1]
+    m = x[2]
+    grad_g = [w, sigma_y, -1]
+    return grad_g
 
 
 def beck_example_2(x, none_variable):
