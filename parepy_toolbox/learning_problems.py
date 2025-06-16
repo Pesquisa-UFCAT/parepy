83ed6e7184223f3bc68698f801bc4951a08a9397
GabrielAnjos2003 <gabriel.costa@discente.ufcat.edu.br>
2025-06-16 17:36:18 -0300

    Atualizações exemplos @luiz

--- parepy_toolbox/learning_problems.py
+++ parepy_toolbox/learning_problems.py
@@ -1,17 +1,19 @@
 """Learning and problems to use in PAREpy toolbox"""
 from typing import Callable
 
-
-def structural_problems(type_: str, name: str) -> tuple[Callable, list]:
+def structural_problems_1(type_: str, name: str) -> tuple[Callable, list]:
     """
-    This function contain several problems about structural reliability.
+    Define problemas estruturais para análise numérica.
 
-    :param type_: Choose type the algorithm you will use in numerical solution. Supported values: (a) 'sampling' and (b) 'derivative'.
-    :param name: Name of problem. Supported values: (a) 'Chang-p558' - aqui a ref zotero, example 10.5 page 558, (b) xxxxxx, (c) xxxxxx
+    :param type_: Tipo de algoritmo a ser usado na solução numérica. Valores suportados: 
+                  (a) 'sampling' - retorno em lista para métodos de amostragem,
+                  (b) 'derivative' - retorno direto para métodos determinísticos.
+    :param name: Nome do problema. Valores suportados: 
+                 (a) 'Chang-p558' - exemplo do livro de Chang, pág. 558,
+                 (b) 'NowakCollins-p123' - exemplo do livro Reliability of Structures, pág. 123,
+                 (c) 'NowakCollins-p129' - exemplo do livro Reliability of Structures, pág. 129.
 
-    :return output[0] = The objective function, output[1] = Containing the distribution type and parameters.
-
-    # Examples
+    :return: output[0] = função objetivo, output[1] = lista com tipo de distribuição e parâmetros das variáveis aleatórias.
     """
 
     if type_ == 'sampling':
@@ -23,9 +25,27 @@ def structural_problems(type_: str, name: str) -> tuple[Callable, list]:
             d = {'type': 'normal', 'parameters': {'mean': 1., 'std': 0.1}}
             l = {'type': 'normal', 'parameters': {'mean': 10., 'std': 1.}}
             random_var_settings = [d, l]
+
         elif name == 'NowakCollins-p123':
-            pass
-    else:
+            def obj_(x):
+                r, q = x
+                return [r - q]
+            obj = obj_
+            r = {'type': 'lognormal', 'parameters': {'mean': 200, 'std': 20}}
+            q = {'type': 'gumbel max', 'parameters': {'mean': 100, 'std': 12}}
+            random_var_settings = [r, q]
+
+        elif name == 'NowakCollins-p129':
+            def obj_(x):
+                z, fy, m = x
+                return [z * fy - m]
+            obj = obj_
+            z = {'type': 'normal', 'parameters': {'mean': 100, 'std': 0.04 * 100}}
+            fy = {'type': 'lognormal', 'parameters': {'mean': 40, 'std': 0.10 * 40}}
+            m = {'type': 'gumbel max', 'parameters': {'mean': 2000, 'std': 0.10 * 2000}}
+            random_var_settings = [z, fy, m]
+
+    elif type_ == 'derivative':
         if name == 'Chang-p558':
             def obj_(x):
                 g_0 = 12.5 * x[0] ** 3 - x[1]
@@ -34,7 +54,28 @@ def structural_problems(type_: str, name: str) -> tuple[Callable, list]:
             d = {'type': 'normal', 'parameters': {'mean': 1., 'std': 0.1}}
             l = {'type': 'normal', 'parameters': {'mean': 10., 'std': 1.}}
             random_var_settings = [d, l]
-        elif name == 'NowakCollins-p123':
-            pass
 
-    return obj, random_var_settings  
+        elif name == 'NowakCollins-p123':
+            def obj_(x):
+                r, q = x
+                return r - q
+            obj = obj_
+            r = {'type': 'lognormal', 'parameters': {'mean': 200, 'std': 20}}
+            q = {'type': 'gumbel max', 'parameters': {'mean': 100, 'std': 12}}
+            random_var_settings = [r, q]
+
+        elif name == 'NowakCollins-p129':
+            def obj_(x):
+                z, fy, m = x
+                return z * fy - m
+            obj = obj_
+            z = {'type': 'normal', 'parameters': {'mean': 100, 'std': 0.04 * 100}}
+            fy = {'type': 'lognormal', 'parameters': {'mean': 40, 'std': 0.10 * 40}}
+            m = {'type': 'gumbel max', 'parameters': {'mean': 2000, 'std': 0.10 * 2000}}
+            random_var_settings = [z, fy, m]
+
+    else:
+        raise ValueError("Tipo de algoritmo inválido. Use 'sampling' ou 'derivative'.")
+
+    return obj, random_var_settings
+
