6001c61b151dcb767c5ad19e45e1cef2fc639c63
Prof. Wanderlei Malaquias Pereira Junior <55900120+wmpjrufg@users.noreply.github.com>
2025-06-15 21:43:51 -0300

    feat: weibull min distribution

--- local_examples/001-sampling.py
+++ local_examples/001-sampling.py
@@ -1,15 +1,16 @@
 import sys
-path  = r'C:\git-projetos\parepy'  # PC notebook wander
+path = r'C:\git-projetos\parepy'  # PC notebook wander
+path = r'D:\py\parepy'
 sys.path.append(path)
 from parepy_toolbox import *
 
 def obj(x):
-    return [12.5 * x[0] ** 3 - x[1]]
+    return [12.5 * x[0] ** 3 - x[1], 12.5 * x[0] ** 3 - x[1]]
 
 d = {'type': 'normal', 'parameters': {'mean': 1., 'std': 0.1}}
 l = {'type': 'normal', 'parameters': {'mean': 10., 'std': 1.}}
 var = [d, l]
-df, pf, beta = sampling_algorithm_structural_analysis(obj, var, 'lhs', 1000, 1, parallel=False, verbose=False)
+df, pf, beta = sampling_algorithm_structural_analysis(obj, var, 'lhs', 1000, 2, parallel=False, verbose=False)
 print(pf)
-print(beta)
+print(beta['beta_0'].iloc[0])
 print(df.head())
